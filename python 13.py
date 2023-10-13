ddaa4dc160096eaf5a7f1589956bc17ff5a97f5f
pkirtan2 <97022033+pkirtan2@users.noreply.github.com>
2023-10-13 12:19:41 +0530

    initial setup

---
+++ python 13.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import re
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(338, 395)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("pyshine.bmp"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.mw  = MainWindow
+
+if _name_ == "_main_":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
